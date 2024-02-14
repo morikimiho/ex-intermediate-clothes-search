e26694570c5616ccf8f128f3e0c64afc133eb80e
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2024-02-14 17:03:28 +0900

    first commit

---
+++ src/main/java/com/example/exintermediatesearchclothes/ExIntermediateSearchClothesApplication.java
@@ -0,0 +1,13 @@
+package com.example.exintermediatesearchclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExIntermediateSearchClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExIntermediateSearchClothesApplication.class, args);
+	}
+
+}
