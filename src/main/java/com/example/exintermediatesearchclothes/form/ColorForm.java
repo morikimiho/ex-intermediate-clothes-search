23f51a013bddab447d809d19ca33aa4a04d164a7
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2024-02-14 17:12:47 +0900

    modelとformの作成

---
+++ src/main/java/com/example/exintermediatesearchclothes/form/ColorForm.java
@@ -0,0 +1,19 @@
+package com.example.exintermediatesearchclothes.form;
+
+public class ColorForm {
+    /** 色 */
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "ColorForm [color=" + color + "]";
+    }
+}
---
+++ src/main/java/com/example/exintermediatesearchclothes/form/GenderForm.java
@@ -0,0 +1,20 @@
+package com.example.exintermediatesearchclothes.form;
+
+public class GenderForm {
+    /** 性別 */
+    private Integer gender;
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "GenderForm [gender=" + gender + "]";
+    }
+
+}
